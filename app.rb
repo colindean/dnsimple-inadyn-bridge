babb2746a03abef885adff48094ac30748bef7c8
Colin Dean <git@cad.cx>
2014-05-07 18:05:35 -0400

    Log only a part of the hash of an email
    I really don't need to know email addresses, other than for troubleshooting.

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'bundler'
 Bundler.require
 require 'json'
 require 'logger'
+require 'digest'
 
 LOGGER = Logger.new(STDOUT)
 
@@ -11,10 +12,12 @@ get business_url do
   #get the email from username and the api_token from password
   @auth ||=  Rack::Auth::Basic::Request.new(request.env)
   unless @auth.provided? && @auth.basic? && @auth.credentials
-    halt 401, "You must supply an email and API token as username and password\n"
+    halt 401,
+         "You must supply an email and API token as username and password.\n"
   end
 
   email, api_token = @auth.credentials
+  ehash = Digest::MD5.hexdigest(email).slice(0..7) #1st 8 chars of hashed email
   domain_id = params[:domain_id]
   record_id = params[:record_id]
   #unused
@@ -26,8 +29,6 @@ get business_url do
   headers = {accept: :json,
              content_type: :json,
              "X-DNSimple-Domain-Token" => "#{api_token}"}
-
-  LOGGER.info "#{email} PUTTING #{ip} TO #{url}"
 
   res = RestClient.put url, payload, headers
 
@@ -45,6 +46,8 @@ get business_url do
   else
     "911"
   end
+
+  LOGGER.info "#{ehash} updating #{domain_id}/#{record_id} to #{ip}"
 
 end
 
